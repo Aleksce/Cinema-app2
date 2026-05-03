c37a15abb35923b22b3630951fbb400a933e0c83
Aleksce <stroimars@bk.ru>
2026-05-03 13:12:48 +0300

    Add CinemaApp/Views/MovieDetailView.xaml.cs

---
+++ CinemaApp/Views/MovieDetailView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace CinemaApp.Views;
+
+public partial class MovieDetailView : UserControl
+{
+    public MovieDetailView()
+    {
+        InitializeComponent();
+    }
+}
